78544a6aba88f622334472a976d55ab04bd2cc13
Piotr Rozponczyk <piotr.rozponczyk@riskhub.co.uk>
2024-06-17 12:05:26 +0100

    Change app title

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Actions'),
+      home: const MyHomePage(title: 'Flutter Actions App'),
     );
   }
 }
